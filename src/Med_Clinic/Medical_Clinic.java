eeeca684410953dbd91d3c01a53c4d193281d96e
janitha000 <janitha000@gmail.com>
2013-10-25 14:09:20 +0530

    Update Medical_Clinic.java

--- src/Med_Clinic/Medical_Clinic.java
+++ src/Med_Clinic/Medical_Clinic.java
@@ -2,6 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+ 
+//janitha
 package Med_Clinic;
 import java.sql.Connection;
 import java.sql.DriverManager;
